0ebf9692af0075e487bc8ad653b584d70204e1da
Sara Presa <sarapresa@ua.pt>
2023-12-26 19:43:32 +0000

    First Commit

---
+++ assets/js/alerta.js
@@ -0,0 +1,86 @@
+// Alert CSS Style
+const style = document.createElement("style")
+
+style.innerHTML = `
+.success-alert, .error-alert {
+    position: fixed;
+    top: 20px;
+    right: 20px;
+    padding: 20px;
+    color: white;
+    text-align: center;
+    border-radius: 15px;
+    box-shadow: 0 2px 5px rgba(0,0,0,0.3);
+    transition: opacity 0.5s ease;
+    opacity: 0;
+}
+
+.success-alert {
+    background-color: #4CAF50;
+}
+
+.error-alert {
+    background-color: #FF0000;
+}
+
+@keyframes fadeIn {
+    0% {opacity: 0;}
+    100% {opacity: 1;}
+}
+
+@keyframes fadeOut {
+    0% {opacity: 1;}
+    100% {opacity: 0;}
+}
+`
+document.head.appendChild(style)
+
+// Show Alert Message
+export function showAlert(message, type) {
+    // Create a new div element
+    var alertDiv = document.createElement("div")
+
+    // Add classes to the div
+    alertDiv.classList.add(type + "-alert")
+
+    // Add the message to the div
+    alertDiv.textContent = message
+
+    // Add a close button
+    let closeButton = document.createElement("span")
+    closeButton.innerHTML = "×"
+    closeButton.style.float = "right"
+    closeButton.style.cursor = "pointer"
+    closeButton.style.marginLeft = "15px"
+    closeButton.style.fontSize = "22px"
+    closeButton.style.lineHeight = "20px"
+    closeButton.style.color = "white"
+    closeButton.onclick = function () {
+        alertDiv.style.animation = "fadeOut 0.5s forwards"
+    }
+    alertDiv.appendChild(closeButton)
+
+    // Append the div to the body
+    document.body.appendChild(alertDiv)
+
+    // Fade in the alert
+    setTimeout(function () {
+        alertDiv.style.animation = "fadeIn 0.5s forwards"
+    }, 0)
+
+    // Fade out and remove the alert after 3 seconds
+    setTimeout(function () {
+        alertDiv.style.animation = "fadeOut 0.5s forwards"
+        setTimeout(function () {
+            document.body.removeChild(alertDiv)
+        }, 300)
+    }, 2000)
+}
+
+export function showSuccessAlert(message) {
+    showAlert(message, "success")
+}
+
+export function showErrorAlert(message) {
+    showAlert(message, "error")
+}
---
+++ assets/js/firebase.js
@@ -0,0 +1,121 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js"
+import { getDatabase, set, ref, get, child } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js"
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js"
+import { showAlert } from "./alerta.js"
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBB1p7R2VHpcdcaql9UWEf_qzzeMQ4koGE",
+    authDomain: "smart-savings-a369a.firebaseapp.com",
+    databaseURL: "https://smart-savings-a369a-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "smart-savings-a369a",
+    storageBucket: "smart-savings-a369a.appspot.com",
+    messagingSenderId: "646945260742",
+    appId: "1:646945260742:web:1ed62af60e1b85ad9ac3c7",
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
+const db = getDatabase()
+const auth = getAuth(app)
+const dbref = ref(db)
+
+// Constants for selectors
+const RegisterForm = document.getElementById("RegisterForm")
+const LoginForm = document.getElementById("LoginForm")
+
+// Function to handle registration
+const handleRegistration = async (evt) => {
+    evt.preventDefault()
+
+    let username = document.getElementById("username")
+    let firstname = document.getElementById("fname")
+    let lastname = document.getElementById("lname")
+    let email = document.getElementById("email")
+    let password = document.getElementById("password")
+
+    try {
+        const credentials = await createUserWithEmailAndPassword(auth, email.value, password.value)
+        console.log(credentials)
+
+        await set(ref(db, "users/" + credentials.user.uid), {
+            firstname: firstname.value,
+            lastname: lastname.value,
+            username: username.value,
+            email: email.value,
+        })
+
+        showAlert("A sua conta foi criada com sucesso!", "success")
+        setTimeout(() => {
+            window.location.href = "index.html"
+        }, 1000)
+    } catch (error) {
+        handleRegistrationError(error)
+        console.error(error)
+    }
+}
+
+// Function to handle login
+const handleLogin = async (evt) => {
+    evt.preventDefault()
+
+    let email = document.getElementById("email")
+    let password = document.getElementById("password")
+
+    try {
+        const credentials = await signInWithEmailAndPassword(auth, email.value, password.value)
+        console.log(credentials)
+
+        const snapshot = await get(child(dbref, "users/" + credentials.user.uid))
+
+        if (snapshot.exists) {
+            sessionStorage.setItem(
+                "user-info",
+                JSON.stringify({
+                    username: snapshot.val().username,
+                    firstname: snapshot.val().firstname,
+                    lastname: snapshot.val().lastname,
+                })
+            )
+            sessionStorage.setItem("user-creds", JSON.stringify(credentials.user))
+            window.location.href = "./admin/index.html"
+        }
+    } catch (error) {
+        handleLoginError(error)
+        console.error(error)
+    }
+}
+
+// Function to handle registration errors
+const handleRegistrationError = (error) => {
+    var errorCode = error.code
+    if (errorCode === "auth/email-already-in-use") {
+        showAlert("O email que introduziu já se encontra em uso", "error")
+    } else if (errorCode === "auth/invalid-email") {
+        showAlert("Introduza um email válido", "error")
+    } else if (errorCode === "auth/weak-password") {
+        showAlert("A password deve ter pelo menos 6 caracteres", "error")
+    } else {
+        showAlert(error.message, "error")
+    }
+}
+
+// Function to handle login errors
+const handleLoginError = (error) => {
+    var errorCode = error.code
+    if (errorCode === "auth/invalid-email") {
+        showAlert("Introduza um email válido", "error")
+    } else if (errorCode === "auth/invalid-credential") {
+        showAlert("As suas credenciais não são válidas", "error")
+    } else {
+        showAlert(error.message, "error")
+    }
+}
+
+// Event listeners
+if (RegisterForm) {
+    RegisterForm.addEventListener("submit", handleRegistration)
+}
+
+if (LoginForm) {
+    LoginForm.addEventListener("submit", handleLogin)
+}
