cb05617e774be7388983657d42550133d316a5f8
Sara Presa <sarapresa@ua.pt>
2023-12-26 21:26:18 +0000

    Nav Bar Progress

---
+++ admin/js/main.js
@@ -0,0 +1,18 @@
+// Nav Bar Active
+document.addEventListener("DOMContentLoaded", function () {
+    // Fetch the sidebar content
+    fetch("navbar.html")
+        .then((response) => response.text())
+        .then((data) => {
+            document.getElementById("sidebar-container").innerHTML = data
+
+            // Use a variável global currentPageId
+            var currentPageId = window.currentPageId
+
+            // Find the link with the corresponding ID and add the "active" class
+            var activeLink = document.getElementById(currentPageId)
+            if (activeLink) {
+                activeLink.classList.add("active")
+            }
+        })
+})
