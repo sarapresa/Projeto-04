08a0ea9c6b6577e062b914e5c21158ec93ae2b3e
Sara Presa <sarapresa@ua.pt>
2023-12-29 00:29:43 +0000

    html

--- admin/js/firebase.js
+++ admin/js/firebase.js
@@ -1,5 +1,11 @@
 const userCreds = JSON.parse(sessionStorage.getItem("user-creds"))
-const userInfo = JSON.parse(sessionStorage.getItem("user-info"))
+const userInfoJSON = sessionStorage.getItem("user-info") // Armazenar o valor original
+
+let userInfo // Variável para armazenar o objeto userInfo
+
+if (userInfoJSON) {
+    userInfo = JSON.parse(userInfoJSON)
+}
 
 const name = document.getElementById("name")
 const signoutBtn = document.getElementById("signoutbutton")
@@ -11,12 +17,22 @@ const Signout = () => {
 }
 
 const CheckCred = () => {
-    if (!sessionStorage.getItem("user-creds")) window.location.href = "../../index.html"
-    else {
+    if (!sessionStorage.getItem("user-creds") || !userInfo) {
+        window.location.href = "../../index.html"
+    } else {
         name.innerText = `${userInfo.firstname + " " + userInfo.lastname}`
         document.title = `Smart Savings | ${userInfo.firstname + " " + userInfo.lastname}`
     }
 }
 
-window.addEventListener("load", CheckCred)
-signoutBtn.addEventListener("click", Signout)
+document.addEventListener("DOMContentLoaded", function () {
+    CheckCred() // Chama CheckCred quando o DOM é completamente carregado
+
+    // Adiciona um ouvinte de evento de clique ao link
+    signoutBtn.addEventListener("click", function (event) {
+        event.preventDefault() // Evita que o link redirecione imediatamente
+
+        // Chama a função Signout quando o link for clicado
+        Signout()
+    })
+})
