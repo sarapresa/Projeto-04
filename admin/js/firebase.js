2a94f09c97002e62e7c8f6ce77a76649e4e0057c
Sara Presa <sarapresa@ua.pt>
2024-01-02 13:27:12 +0000

    firebase

--- admin/js/firebase.js
+++ admin/js/firebase.js
@@ -1,19 +1,3 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js"
-import { getStorage } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-storage.js"
-
-const firebaseConfig = {
-    apiKey: "AIzaSyBB1p7R2VHpcdcaql9UWEf_qzzeMQ4koGE",
-    authDomain: "smart-savings-a369a.firebaseapp.com",
-    databaseURL: "https://smart-savings-a369a-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "smart-savings-a369a",
-    storageBucket: "smart-savings-a369a.appspot.com",
-    messagingSenderId: "646945260742",
-    appId: "1:646945260742:web:1ed62af60e1b85ad9ac3c7",
-}
-
-const app = initializeApp(firebaseConfig)
-const storage = getStorage(app)
-
 const userCreds = JSON.parse(sessionStorage.getItem("user-creds"))
 const userInfoJSON = sessionStorage.getItem("user-info")
 let userInfo // Variável para armazenar o objeto userInfo
@@ -59,27 +43,3 @@ document.addEventListener("DOMContentLoaded", function () {
         Signout()
     })
 })
-
-const fileInput = document.getElementById("fileInput") // Adicione um input de arquivo no seu HTML com o ID "fileInput"
-
-const uploadProfileImage = () => {
-    const file = fileInput.files[0]
-
-    if (file) {
-        // Obtenha a referência diretamente do storage
-        const storageRef = getStorage().ref(`users/${userCreds.uid}/profile.jpg`)
-
-        storageRef
-            .put(file)
-            .then(() => {
-                console.log("Image uploaded successfully!")
-            })
-            .catch((error) => {
-                console.error("Error uploading image:", error)
-            })
-    }
-}
-
-const uploadButton = document.getElementById("uploadButton")
-
-uploadButton.addEventListener("click", uploadProfileImage)
